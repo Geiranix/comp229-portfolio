0eec0a721f45a8963dde0cbda437bda9c67e14cd
Ayowolemi Eni-omosule <ayowolemieniomosule@gmail.com>
2024-05-31 19:31:02 -0400

    I created the layout file

---
+++ src/pages/Layout.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
+
+function Layout({children}) {
+  return (
+    <div className='w-[100vw] h-[100vh] flex flex-col items-center justify-between'>
+      <Header />
+      <main className='h-[65%] w-[100vw] bg-[#333945] overflow-scroll'>{children}</main>
+      <Footer />
+    </div>
+  )
+}
+
+export default Layout
